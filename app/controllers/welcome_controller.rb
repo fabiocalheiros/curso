e9638f701714a99ad04571336bc4812b06b00922
Fabio Calheiros <fabio.calheiros@gmail.com>
2014-04-02 18:54:11 -0300

    comentarios da aplicação

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,9 +1,12 @@
 class WelcomeController < ApplicationController
 
+	# metodo contrutor da página inicial
 	def index
 		@name = "Fábio Calheiros"
 	end
 
+	# metodo black aponta para um novo layout
+	# o layout deve estar dentro da pasta welcome para que funcione
 	def black
 		@text = "Esta é uma página com novo layout"
 		render layout: 'application_black'
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 FirstApp::Application.routes.draw do
 
+  # quando acessar a url principal vai para o controler welcome e chama a index
   root 'welcome#index'
 
+  # pega o nome da url "black" e atribui a uma difinição no controller welcome
   get 'black', to: 'welcome#black'
   
 end
