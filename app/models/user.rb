e5227876e7a4dbf815e0130bb99a2dc859059a57
Fabio Calheiros <fabio.calheiros@gmail.com>
2014-04-03 20:38:53 -0300

    necessario informar sexo caso idade seja maior que 18

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,21 @@
 class User < ActiveRecord::Base
 
+	# Constants
 	FEMALE = 1;
 	MALE = 2;
+
+	# Validations
 	validates :email, presence: true, allow_blank: false
 	validates :name, presence: true, allow_blank: false
+	validates :gender, presence: true, if: :adulthood
 	#validates_uniqueness_of :email
+
+	# Associations
+
+	# Scopes
+
+	# Public methods
+	def adulthood
+		self.age.present? and self.age > 18
+	end
 end
--- spec/models/users_spec.rb
+++ spec/models/users_spec.rb
@@ -4,7 +4,8 @@ describe User do
 	it 'create a valid user' do
 		user = User.new name: "Fabio Calheiros", 
 						email: "fabio.calheiros@gmail.com", 
-						age: 28
+						age: 28, 
+						gender: User::MALE
 		user.save.should be_true
 	end
 
@@ -23,5 +24,29 @@ describe User do
 						email: "candinha@ig.com.br",
 						gender: User::FEMALE
 		user.save.should be_true
+	end
+
+	context 'when age >= 18' do
+		it 'creates an user with gender' do
+			user = User.new name: "nome", email: "email@email.com.br", age: 19, gender: User::MALE
+			user.save.should be_true
+		end
+
+		it 'does not create with blank gender' do
+			user = User.new name: "nome", email: "email@email.com.br", age: 19
+			user.save.should be_false
+		end
+	end
+
+	context 'when age < 18' do
+		it 'creates an user with gender' do
+			user = User.new name: "nome", email: "email@email.com.br", age: 17, gender: User::MALE
+			user.save.should be_true
+		end
+
+		it 'create with blank gender' do
+			user = User.new name: "nome", email: "email@email.com.br", age: 17
+			user.save.should be_true
+		end
 	end
 end
