b5a9a30b3d9fd2a455baac610a60909ba14e7d0d
Fabio Calheiros <fabio.calheiros@gmail.com>
2014-04-08 19:52:31 -0300

    edição e remoção de contas

--- app/controllers/bills_controller.rb
+++ app/controllers/bills_controller.rb
@@ -10,6 +10,15 @@ class BillsController < ApplicationController
 		@bill.destroy
 	end
 
+	def edit
+		@bill = Bill.find(params[:id])
+	end
+
+	def update
+		@bill = Bill.find(params[:id])
+		@bill.update bill_params
+	end
+
 	private
 
 	def bill_params
